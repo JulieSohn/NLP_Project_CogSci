a3092b89e9f5e4d172de167f6e50921f2b8a3d51
marielouisehm <52203426+marielouisehm@users.noreply.github.com>
2020-01-02 13:31:01 +0100

    Update 6_Analysis_of_sentiment.py
    Deleted wordcloud

--- Scripts/6_Analysis_of_sentiment.py
+++ Scripts/6_Analysis_of_sentiment.py
@@ -215,38 +215,3 @@ def bigramming(noget):
 
 bigramming(sentences_neg)
 bigramming(sentences_pos)
-
-#%%
-#=======================================================
-# WordCloud function
-#=======================================================
-
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-
-def show_wordcloud(data, title = None):
-    wordcloud = WordCloud(
-        background_color = 'white',
-        max_words = 200,
-        max_font_size = 40, 
-        scale = 3,
-        random_state = 42
-    ).generate(str(data))
-
-    fig = plt.figure(1, figsize = (20, 20))
-    plt.axis('off')
-    if title: 
-        fig.suptitle(title, fontsize = 20)
-        fig.subplots_adjust(top = 2.3)
-
-    plt.imshow(wordcloud)
-    plt.show()
-    
-# Print wordcloud
-show_wordcloud(data["sent_toke"])
-
-for i in sentences_neg:
-    show_wordcloud(i)
-
-for i in sentences_pos:
-    show_wordcloud(i)
